d6c23bc461550b20acf384823a0221448693f37f
Eduardo Vasconcelos <dudumottavasconcelos@gmail.com>
2022-05-14 09:10:33 -0300

    Subindo primeiras configurações

---
+++ src/test/java/edu/ifes/ci/si/les/smp/SmpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.smp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
